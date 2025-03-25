e4731926bebdd3128995870fde9c867ee39f2bed
Kristan <itan_07@hotmail.com>
2025-03-24 19:43:38 -0700

    Now program exits when user press ESC

--- test.asm
+++ test.asm
@@ -1,15 +1,15 @@
 INCLUDE krsv2.inc
 data segment
-menu_line1 db "Men", 0A3h, " de Cadenas", 0
-menu_line2 db "1. ", 0A8h, "Es pal", 0A1h, "ndromo?", 0
-menu_line3 db "2. N", 0A3h, "mero de vocales", 0
-menu_line4 db "3. Obtener subcadena", 0
-menu_line5 db "4. ?", 0
-menu_line6 db "<ESC> Para Salir", 0
-input_prompt db "Enter your choice: ", 0
+    menu_line1 db "Men", 0A3h, " de Cadenas", 0
+    menu_line2 db "1. ", 0A8h, "Es pal", 0A1h, "ndromo?", 0
+    menu_line3 db "2. N", 0A3h, "mero de vocales", 0
+    menu_line4 db "3. Obtener subcadena", 0
+    menu_line5 db "4. ?", 0
+    menu_line6 db "<ESC> Para Salir", 0
+    input_prompt db "Enter your choice: ", 0
 
-; Define the input buffer (20 characters + 1 for null terminator)
-input_buffer db 21 dup(0)  ; Reserve 21 bytes, initialized to 0
+; Define the input buffer (5 characters + 1 for null terminator)
+    input_buffer db 6 dup(0)  ; Reserve 6 bytes, initialized to 0
 data ends
 
 code segment
@@ -19,11 +19,14 @@ code segment
         mov ax, data
         mov ds, ax
         ;=== Start Code ===
+@MAIN:
         Theme_SetRed
         Draw_Menu 0 0 20 10
         Console_WriteBlankLine
-        Console_WriteBlankLine
-        Console_WriteBlankLine
+        mov al, input_buffer[0]
+        cmp al, 27
+        jne @MAIN
+        
         ;=== End Code ====
         App_Exit
 code ends
