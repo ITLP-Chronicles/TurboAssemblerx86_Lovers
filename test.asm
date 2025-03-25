980d13ba6b9b47ebcab25cd22057f9918a8ccd19
Kristan <itan_07@hotmail.com>
2025-03-24 21:10:51 -0700

    Before implement menu

--- test.asm
+++ test.asm
@@ -7,7 +7,7 @@ data segment
     menu_line4 db "3. Obtener subcadena", 0
     menu_line5 db "4. ?", 0
     menu_line6 db "<ESC> Para Salir", 0
-    input_prompt db "Enter your choice: ", 0
+    input_prompt db "Selecci", "o" ,"n: ", 0
 
     ; Define the input buffer (5 characters + 1 for null terminator)
     input_buffer db 6 dup(0)  ; Reserve 6 bytes, initialized to 0
@@ -32,25 +32,24 @@ data segment
     vowel_input db 16 dup(0)  ; Reserve 16 bytes, initialized to 0
     
     
-    
-; Define the strings (null-terminated) with special characters
-substring_line1 db "Obtener subcadena", 0
-substring_line2 db "Cadena: ", 0
-substring_line3 db "Inicio: ", 0
-substring_line4 db "T", 0A2h, "rmino: ", 0  ; T?rmino (? = 0A2h in code page 437)
-substring_line5 db "Subcadena = ", 0
+    ;================= substring ========================
+    ; Define the strings (null-terminated) with special characters
+    substring_line1 db "Obtener subcadena", 0
+    substring_line2 db "Cadena: ", 0
+    substring_line3 db "Inicio: ", 0
+    substring_line4 db "T", 0A2h, "rmino: ", 0  ; T?rmino (? = 0A2h in code page 437)
+    substring_line5 db "Subcadena = ", 0
 
-; Define the input buffers
-substring_input db 21 dup(0)  ; Reserve 21 bytes for the input string (20 chars + null)
-start_input db 3 dup(0)       ; Reserve 3 bytes for start index (2 chars + null)
-end_input db 3 dup(0)         ; Reserve 3 bytes for end index (2 chars + null)
+    ; Define the input buffers
+    substring_input db 21 dup(0)  ; Reserve 21 bytes for the input string (20 chars + null)
+    start_input db 3 dup(0)       ; Reserve 3 bytes for start index (2 chars + null)
+    end_input db 3 dup(0)         ; Reserve 3 bytes for end index (2 chars + null)
 
-; Define the result buffer (max 15 characters + 1 for null terminator)
-substring_result db 16 dup(0)  ; Reserve 16 bytes for the substring
-    
+    ; Define the result buffer (max 15 characters + 1 for null terminator)
+    substring_result db 16 dup(0)  ; Reserve 16 bytes for the substring
 data ends
 
-code segment
+code segment 
     .386
     start:
         assume cs:code, ds:data
@@ -59,17 +58,20 @@ code segment
         ;=== Start Code ===
 @MAIN:
         Theme_SetRed
-        ;Draw_Menu 0 0 20 10
+        Draw_Menu 0 0 20 10
+        mov al, input_buffer[0]
+        cmp al, '1'
+        ;je
         ;Console_WriteBlankLine
         
-        Draw_SubstringInput 10 10
-        Console_WriteBlankLine
-        ;mov al, input_buffer[0]
-        ;cmp al, 27
-
-        ;jne @MAIN
+        ;Draw_SubstringInput 10 10 
+        ;Console_WriteBlankLine
+       
         
         ;=== End Code ====
         App_Exit
+
+;====================================== PROCEDURES =====================================
+
 code ends
 end start
