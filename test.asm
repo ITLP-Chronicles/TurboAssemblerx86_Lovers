9292411b7bd0f6d9378f0279ce1394cecb6b9542
Kristan <itan_07@hotmail.com>
2025-03-24 19:46:15 -0700

    now palindrome has a square!

--- test.asm
+++ test.asm
@@ -1,5 +1,6 @@
 INCLUDE krsv2.inc
 data segment
+    ;MENU
     menu_line1 db "Men", 0A3h, " de Cadenas", 0
     menu_line2 db "1. ", 0A8h, "Es pal", 0A1h, "ndromo?", 0
     menu_line3 db "2. N", 0A3h, "mero de vocales", 0
@@ -8,8 +9,18 @@ data segment
     menu_line6 db "<ESC> Para Salir", 0
     input_prompt db "Enter your choice: ", 0
 
-; Define the input buffer (5 characters + 1 for null terminator)
+    ; Define the input buffer (5 characters + 1 for null terminator)
     input_buffer db 6 dup(0)  ; Reserve 6 bytes, initialized to 0
+    
+    
+    
+    ;Palindrome menu
+    palindrome_line1 db 0A8h, "Es pal", 0A1h, "ndromo?", 0
+    palindrome_line2 db "Cadena: ", 0
+    palindrome_line3 db "S", 0A1h, " es Pal", 0A1h, "ndromo", 0
+
+    ; Define the input buffer (15 characters + 1 for null terminator)
+    palindrome_input db 16 dup(0)  ; Reserve 16 bytes, initialized to 0
 data ends
 
 code segment
@@ -21,10 +32,14 @@ code segment
         ;=== Start Code ===
 @MAIN:
         Theme_SetRed
-        Draw_Menu 0 0 20 10
+        ;Draw_Menu 0 0 20 10
+        ;Console_WriteBlankLine
+        
+        Draw_PalindromeCheck 10 10
         Console_WriteBlankLine
         mov al, input_buffer[0]
         cmp al, 27
+
         jne @MAIN
         
         ;=== End Code ====
